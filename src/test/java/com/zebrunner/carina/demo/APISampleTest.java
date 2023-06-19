4bbb9b01c7c448bb89054054e0badc428235d97a
jjanickamatraszek <jjanickamatraszek@solvd.com>
2023-06-19 23:05:38 +0200

    test commit

--- src/test/java/com/zebrunner/carina/demo/APISampleTest.java
+++ src/test/java/com/zebrunner/carina/demo/APISampleTest.java
@@ -44,53 +44,53 @@ public class APISampleTest implements IAbstractTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Test()
-    //@MethodOwner(owner = "qpsdemo")
+    @MethodOwner(owner = "qpsdemo")
     public void testCreateUser() throws Exception {
-//        LOGGER.info("test");
-//        setCases("4555,54545");
-//        PostUserMethod api = new PostUserMethod();
-//        api.setProperties("api/users/user.properties");
-//
-//        AtomicInteger counter = new AtomicInteger(0);
-//
-//        api.callAPIWithRetry()
-//                .withLogStrategy(APIMethodPoller.LogStrategy.ALL)
-//                .peek(rs -> counter.getAndIncrement())
-//                .until(rs -> counter.get() == 4)
-//                .pollEvery(1, ChronoUnit.SECONDS)
-//                .stopAfter(10, ChronoUnit.SECONDS)
-//                .execute();
-//        api.validateResponse();
+        LOGGER.info("test");
+        setCases("4555,54545");
+        PostUserMethod api = new PostUserMethod();
+        api.setProperties("api/users/user.properties");
+
+        AtomicInteger counter = new AtomicInteger(0);
+
+        api.callAPIWithRetry()
+                .withLogStrategy(APIMethodPoller.LogStrategy.ALL)
+                .peek(rs -> counter.getAndIncrement())
+                .until(rs -> counter.get() == 4)
+                .pollEvery(1, ChronoUnit.SECONDS)
+                .stopAfter(10, ChronoUnit.SECONDS)
+                .execute();
+        api.validateResponse();
     }
 
-//    @Test()
-//    @MethodOwner(owner = "qpsdemo")
-//    public void testCreateUserMissingSomeFields() throws Exception {
-//        PostUserMethod api = new PostUserMethod();
-//        api.setProperties("api/users/user.properties");
-//        api.getProperties().remove("name");
-//        api.getProperties().remove("username");
-//        api.callAPIExpectSuccess();
-//        api.validateResponse();
-//    }
-//
-//    @Test()
-//    @MethodOwner(owner = "qpsdemo")
-//    public void testGetUsers() {
-//        GetUserMethods getUsersMethods = new GetUserMethods();
-//        getUsersMethods.callAPIExpectSuccess();
-//        getUsersMethods.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey());
-//        getUsersMethods.validateResponseAgainstSchema("api/users/_get/rs.schema");
-//    }
-//
-//    @Test()
-//    @MethodOwner(owner = "qpsdemo")
-//    @TestPriority(Priority.P1)
-//    public void testDeleteUsers() {
-//        DeleteUserMethod deleteUserMethod = new DeleteUserMethod();
-//        deleteUserMethod.setProperties("api/users/user.properties");
-//        deleteUserMethod.callAPIExpectSuccess();
-//        deleteUserMethod.validateResponse();
-//    }
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    public void testCreateUserMissingSomeFields() throws Exception {
+        PostUserMethod api = new PostUserMethod();
+        api.setProperties("api/users/user.properties");
+        api.getProperties().remove("name");
+        api.getProperties().remove("username");
+        api.callAPIExpectSuccess();
+        api.validateResponse();
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    public void testGetUsers() {
+        GetUserMethods getUsersMethods = new GetUserMethods();
+        getUsersMethods.callAPIExpectSuccess();
+        getUsersMethods.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey());
+        getUsersMethods.validateResponseAgainstSchema("api/users/_get/rs.schema");
+    }
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    public void testDeleteUsers() {
+        DeleteUserMethod deleteUserMethod = new DeleteUserMethod();
+        deleteUserMethod.setProperties("api/users/user.properties");
+        deleteUserMethod.callAPIExpectSuccess();
+        deleteUserMethod.validateResponse();
+    }
 
 }
